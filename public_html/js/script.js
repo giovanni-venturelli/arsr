b91a7684a22478bf06a83e303c41d6b85d644029
uomoermeneutico <uomoermeneutico@libero.it>
2015-07-01 02:12:31 +0200

    aggiornati login.xml e script.js

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -16,27 +16,43 @@ function checkEmail(){
 }
 
 function checkNotEmpty(inp){
-    var mess=document.getElementById(inp.id+"_check")
-    if (inp.value == '')
+    var mess=document.getElementById(inp.id+"_check");
+    var val = false;
+    if (inp.value == '' && inp.id!="cognome")
     {
         inp.style.borderColor = "red";
         mess.style.display="inline-block";
+        val=false;
+
+    }
+    else if (inp.value == '' && inp.id==="cognome" && !document.getElementById("seleziona_partita_iva").checked)
+    {
+        inp.style.borderColor = "red";
+        mess.style.display="inline-block";
+        val=false;
+
     }
     else
     {
      inp.style.borderColor = "#2CDC2C";
      mess.style.display="none";
+     val = true;
+
  }
+ return val;
 }
 
 function validateForm(form){
     var valid=true;
     var el=form.elements;
-    for (var i=0; i<el.length && valid===true; i++) {
-        if (el[i].value===''){
+    for (var i=0; i<el.length; i++) {
+        var temp=true;
+        if(el[i].nodeName!="FIELDSET" && el[i].type!="submit" && el[i].type!="radio" && el[i].type!="reset"){
+        temp=checkNotEmpty(el[i]);
+        if(temp===false){
             valid=false;
-            window.alert("Devi riempire tutti i campi!");
         }
+    }
     }
     return valid; 
 }
@@ -68,11 +84,13 @@ function checkNumber(inp){
     {
         inp.style.borderColor = "red";
         mess.style.display="inline-block";
+
     }
     else
     {
      inp.style.borderColor = "#2CDC2C";
      mess.style.display="none";
+
  }
 }
 
@@ -93,4 +111,18 @@ function checkDayNumber(inp){
      valid=true;
  }
  return valid;
+}
+
+function handleCognome(){
+    var dis = document.getElementById("cognome");
+    var par = document.getElementById("seleziona_partita_iva");
+    var cod = document.getElementById("seleziona_codice");
+    if(par.checked){
+        dis.setAttribute("disabled", "disabled");
+        checkNotEmpty(dis);
+    }
+    else{
+        dis.removeAttribute("disabled");
+        checkNotEmpty(dis);
+    }
 }
